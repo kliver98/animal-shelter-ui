dbcc6d589a4cf8d53e4d3832d8ae51f964554d87
Kliver Daniel Giron Castro <kliver.gironc@perficient.com>
2021-07-21 15:27:04 -0500

    fix: fix desc

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/kliver98/animal-shelter-ui",
+    plugins: ['@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github'
+    ]
+}
